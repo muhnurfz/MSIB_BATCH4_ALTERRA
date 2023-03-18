0d279dd7816ad966b0850f6aff54c211ecb71845
MuhamadNurFajjriZenitandrio <muhnurfz@gmail.com>
2023-03-19 06:49:20 +0700

    update file

--- 6-Basic_Programming_&_OOP/Praktikum/perhitungan_untungrugi.java
+++ 6-Basic_Programming_&_OOP/Praktikum/perhitungan_untungrugi.java
@@ -5,14 +5,15 @@ public class perhitungan_untungrugi {
         Scanner input = new Scanner(System.in);
         System.out.println("Perhitungan untung rugi");
         System.out.println("Muhamad Nur Fajjri Z-Kelas D QE 2023    ");
-        System.out.println("Masukan nilai pertama       : ");
+        System.out.println("Masukan harga beli barang (modal)       : ");
         double num1 = input.nextDouble();
-        System.out.println("Masukan nilai kedua         : ");
+        System.out.println("Masukan harga jual barang               : ");
         double num2 = input.nextDouble();
 
-        double result = 0;
-        result = num1 - num2;
-
-        System.out.println("Maka hasil perhitunganya adalah   : " + result);
+        if (num1<num2) {
+            System.out.println("Anda mendapat keuntungan" +(num2-num1));
+        } else {
+            System.out.println("Anda mendapat kerugian"+ (num2-num1));
+        }
     }
 }
