5f6aae0d1e3c0fc9c50d0eba1ee645c9d0f461ae
MuhamadNurFajjriZenitandrio <muhnurfz@gmail.com>
2023-03-19 08:40:07 +0700

    update file

---
+++ 6-Basic_Programming_&_OOP/Praktikum/calc/calc.java
@@ -0,0 +1,22 @@
+public class calc {
+    public static void main(String[] args) {
+    public int add(int x, int y){
+        int hasil = x+y;
+        return hasil;
+    }
+        public int subtract(int x, int y){
+            int hasil = x-y;
+            return hasil;
+        }
+
+        public int multiply(int x, int y){
+            int hasil = x*y;
+            return hasil;
+        }
+
+        public int divde(int x, int y){
+            int hasil = x/y;
+            return hasil;
+        }
+
+    }
---
+++ 6-Basic_Programming_&_OOP/Praktikum/calc/main.java
@@ -0,0 +1,8 @@
+class main {
+    public static void main(String[] args) {
+        System.out.println(calc.add (x 3, y 4));
+        System.out.println(calc.subtract (x 15, y 4));
+        System.out.println(calc.multiply (x 10, y 10));
+        System.out.println(calc.divide (x 12, y 3));
+    }
+}
